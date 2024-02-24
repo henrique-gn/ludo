ab63f11ca1e0ee18ac8e6fde33617858b54bad23
henrique-gn <dev.henriquegn@gmail.com>
2024-02-24 20:12:26 -0300

    feat: add error filter

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,25 @@
 import fastify from "fastify";
 import { applyCreateGame } from "./controllers/create-game";
 import { applyGetStatus } from "./controllers/get-status";
+import { ZodError } from "zod";
 
 const app = fastify();
 
 applyCreateGame(app);
 applyGetStatus(app);
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: error.issues[0].message,
+    });
+  }
+
+  return reply.status(500).send({
+    message: "Erro interno",
+    details: error.message,
+  });
+});
 
 const port = 3000;
 const host = "localhost";
